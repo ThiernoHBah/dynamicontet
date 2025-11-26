918c4e36e2e37509101f0daecaf4eaa2d41a9681
thiernohbah <thiernobah2001@gmail.com>
2025-11-26 09:41:03 -0500

    created a function that allows yout ot otal the price when entered

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const productPriceInput = document.getElementById("productPrice");
 const addProductButton = document.getElementById("addProduct");
 const cart = document.getElementById("cart");
 const totalPriceSpan = document.getElementById("totalPrice");
+let totalPrice = 0;
+
+// Update total price display
+function updateTotalPriceDisplay() {
+  totalPriceSpan.textContent = totalPrice.toFixed(2);
+}
