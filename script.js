c94bb235b5d1c4f0b186921553723a69145e9eff
thiernohbah <thiernobah2001@gmail.com>
2025-11-26 09:39:21 -0500

    starting on my js file adentified my elemetsbyid

---
+++ script.js
@@ -0,0 +1,5 @@
+const productNameInput = document.getElementById("productName");
+const productPriceInput = document.getElementById("productPrice");
+const addProductButton = document.getElementById("addProduct");
+const cart = document.getElementById("cart");
+const totalPriceSpan = document.getElementById("totalPrice");
